16ea532eec63c5c2ce173e85cee5688f3a2b6a26
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2024-03-09 21:42:13 -0700

    getquestionin answers page Add questiondescription

--- controller/answerController.js
+++ controller/answerController.js
@@ -36,8 +36,9 @@ async function getAnswerForQuestion(req, res) {
     const { questionid } = req.params; 
     try {
         const answers = await dbConnection.query(`
-            SELECT a.*, u.username AS username
+            SELECT a.*, q.questiondescription, u.username AS username
             FROM answerTable a
+            INNER JOIN questions q ON a.questionid = q.questionid
             INNER JOIN users u ON a.usersid = u.usersid
             WHERE a.questionid = ?
             ORDER BY a.answerid DESC
@@ -51,37 +52,8 @@ async function getAnswerForQuestion(req, res) {
         console.error(error.message);
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Something went wrong while fetching answers" });
     }
-    
-
 }
 
-
-// const postAnswer = async (req, res) => {
-//     const questionId = req.params.questionid;
-//     console.log("Question ID:", questionId); 
-
-//     try {
-//         let questionAndAnswers = await dbConnection.query(`
-//             SELECT * 
-//             FROM answertable 
-//             WHERE questionid = '${questionId}' 
-//             ORDER BY answerid DESC
-//         `);
-
-//         if (questionAndAnswers[0].length === 0) {
-//             console.log("No answers found for question ID:", questionId);
-//         }
-
-//         res.status(200).json({
-//             status: true,
-//             total: questionAndAnswers[0].length,
-//             answers: questionAndAnswers[0],
-//         });
-//     } catch (error) {
-//         console.error("Error retrieving answers:", error);
-//         return res.status(500).json({ status: false, message: "Internal server error" });
-//     }
-// }
 
 
 module.exports = { postAnswer,  getAnswerForQuestion};
